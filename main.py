ae26b5cc1517f4940664e6a6c35dca708b50fb7b
Jake Master <agarpunch@gmail.com>
2022-12-12 23:44:38 +1100

    New sync command

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import traceback
 import discord
 from discord.ext import commands
+from discord.ext.commands import Greedy, Context
+from typing import Literal, Optional
 from discord import app_commands
 from random import randint
 import platform
@@ -413,11 +415,38 @@ async def on_guild_remove(ctx, error):
     
     
 @bot.command()
+@commands.guild_only()
 @commands.is_owner()
-async def sync(ctx):
-    tree.copy_global_to(guild=ctx.guild)
-    await tree.sync(guild=ctx.guild)
-    await ctx.reply("Tree synced")
+async def sync(
+  ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
     
 @bot.command()
 @commands.is_owner()
